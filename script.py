f98600f72815d0d1a841e4985d1cd1ae1d1aaae7
joaoemmanuel <joao.basass@gmail.com>
2025-09-16 21:45:27 -0300

    Including the first bits of code

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
-print ("Hello World")
+import requests
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+
+POLYGON_API_KEY = os.getenv('POLYGON_API_KEY')
+
+LIMIT = 5
+url = f'https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}'
+response = requests.get(url)
+data = response.json()
+print(data['results']["ticker"])
